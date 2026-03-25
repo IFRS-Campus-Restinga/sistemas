e3b2b7d713a2976b2a392471220d100629f0926f
Cássio Renan Rodrigues de Paula <cassio.rodrigues@skailhq.com>
2026-03-24 21:20:57 -0300

    ajuste da estrutura das branches e workflow de PR para staging

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,7 +5,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base: '/',
   build: {
-    outDir: 'static',
+    outDir: '../backend/static',
+    emptyOutDir: true,
   },
   plugins: [react()],
 })
